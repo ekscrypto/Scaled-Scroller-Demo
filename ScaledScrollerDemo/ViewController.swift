1c402790d10bbb92cd4660c6d0733b50e4b56ae0
Dave Poirier <dave@idfusion.com>
2018-08-10 19:43:03 -0400

    Completed sample
    Signed-off-by: Dave Poirier <dave@idfusion.com>

--- ScaledScrollerDemo/ViewController.swift
+++ ScaledScrollerDemo/ViewController.swift
@@ -3,8 +3,10 @@
 //  ScaledScrollerDemo
 //
 //  Created by Dave Poirier on 2018-08-10.
-//  Copyright © 2018 Soft.Io. All rights reserved.
+//  Unlicensed - Public Domain
 //
+//  Answer to Stackoverflow question: https://stackoverflow.com/questions/51792730/how-to-make-full-screen-background-image-inside-scrollview-and-keep-aspect-ratio
+
 
 import UIKit
 
@@ -20,6 +22,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func openLevel1(_: Any?) {
+        print("Level 1 requested!")
+    }
 
+    @IBAction func openLevel9(_: Any?) {
+        print("Level 9 requested!")
+    }
+
+    @IBAction func openLevel10(_: Any?) {
+        print("Level 10 requested!")
+    }
 }
 
