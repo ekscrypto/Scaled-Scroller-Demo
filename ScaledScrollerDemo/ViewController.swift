5e0554a48cc018587d95c18ef549b477d56348c3
Dave Poirier <dave@idfusion.com>
2018-08-10 18:53:16 -0400

    Initial Commit

---
+++ ScaledScrollerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScaledScrollerDemo
+//
+//  Created by Dave Poirier on 2018-08-10.
+//  Copyright © 2018 Soft.Io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
